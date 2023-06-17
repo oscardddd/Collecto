7132042c396f53f00937a63be5cfd8f59dfd47b2
oscarddddddd <qianlidong0@gmail.com>
2023-06-16 21:21:49 -0500

    chat

--- index.js
+++ index.js
@@ -15,62 +15,71 @@ client.on('ready', () => {
 });
 
 const configuration = new Configuration({
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
-  if (message.channel.id !== process.env.CHANNEL_ID) return;
-  if (message.content.startsWith('!')) return;
-
-  let conversationLog = [
-    { role: 'system', content: 'You are a friendly chatbot.' },
-  ];
-
-  try {
-    await message.channel.sendTyping();
-    let prevMessages = await message.channel.messages.fetch({ limit: 15 });
-    prevMessages.reverse();
-    
-    prevMessages.forEach((msg) => {
-      if (msg.content.startsWith('!')) return;
-      if (msg.author.id !== client.user.id && message.author.bot) return;
-      if (msg.author.id == client.user.id) {
-        conversationLog.push({
-          role: 'assistant',
-          content: msg.content,
-          name: msg.author.username
-            .replace(/\s+/g, '_')
-            .replace(/[^\w\s]/gi, ''),
-        });
-      }
-
-      if (msg.author.id == message.author.id) {
-        conversationLog.push({
-          role: 'user',
-          content: msg.content,
-          name: message.author.username
-            .replace(/\s+/g, '_')
-            .replace(/[^\w\s]/gi, ''),
-        });
-      }
-    });
-
-    const result = await openai
-      .createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: conversationLog,
-        // max_tokens: 256, // limit token usage
-      })
-      .catch((error) => {
-        console.log(`OPENAI ERR: ${error}`);
-      });
-    message.reply(result.data.choices[0].message);
-  } catch (error) {
-    console.log(`ERR: ${error}`);
+  // if (message.channel.id !== process.env.CHANNEL_ID) return;
+  // if (message.content.startsWith('!')) return;
+  if(message.content === 'ping'){
+    message.reply('pong');
   }
+  if(message.content.startsWith('!')){
+    let conversationLog = [
+      { role: 'system', 
+      content: 'You are a script writer who would generate story topics relevant to the user input' },
+  
+    ];
+    try {
+      await message.channel.sendTyping();
+      let prevMessages = await message.channel.messages.fetch({ limit: 5 });
+      prevMessages.reverse();
+      
+      prevMessages.forEach((msg) => {
+        // if (msg.content.startsWith('!')) return;
+        if (msg.author.id !== client.user.id && message.author.bot) return;
+        if (msg.author.id == client.user.id) {
+          conversationLog.push({
+            role: 'assistant',
+            content: msg.content,
+            name: msg.author.username
+              .replace(/\s+/g, '_')
+              .replace(/[^\w\s]/gi, ''),
+          });
+        }
+  
+        if (msg.author.id == message.author.id) {
+          conversationLog.push({
+            role: 'user',
+            content: msg.content,
+            name: message.author.username
+              .replace(/\s+/g, '_')
+              .replace(/[^\w\s]/gi, ''),
+          });
+        }
+      });
+  
+      const result = await openai
+        .createChatCompletion({
+          model: 'gpt-3.5-turbo',
+          messages: conversationLog,
+          // max_tokens: 256, // limit token usage
+        })
+        .catch((error) => {
+          console.log(`OPENAI ERR: ${error}`);
+        });
+      message.reply(result.data.choices[0].message);
+    } catch (error) {
+      console.log(`ERR: ${error}`);
+    }
+  }
+
+  
+
+ 
 });
 
 client.login(process.env.TOKEN);
